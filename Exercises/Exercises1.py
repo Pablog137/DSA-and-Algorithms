67007129cededce5565390ebf925e258cf068a30
Pablo <pablogutierrezvaldin@gmail.com>
2024-10-11 17:54:28 +0200

    Exercise 2

--- Exercises/Exercises1.py
+++ Exercises/Exercises1.py
@@ -1,3 +1,5 @@
+### ! EXERCISE 1
+
 """Complete the method/function so that it converts dash/underscore delimited words into camel casing.
 The first word within the output should be capitalized only if the original word was capitalized.
 The next words should be always capitalized.
@@ -30,3 +32,41 @@ example_2 = "The_Stealth_Warrior"
 # print(to_camel_case(example_1))
 
 
+### ! EXERCISE 2
+
+"""In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Examples : 
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+"""
+
+# Solution 1
+
+
+def filter_list(listX: list):
+    result_list = []
+
+    if listX.count == 0:
+        return listX
+
+    for i in listX:
+        if type(i) is int:
+            result_list.append(i)
+
+    return result_list
+
+
+# Solution 2
+
+
+def filter_list2(listX: list):
+    return list(filter(lambda x: type(x) is int, listX))
+
+
+# print(filter_list2([1, 2, "a", "b"]))
+# print(filter_list2([1, "a", "b", 0, 15]))
+# print(filter_list2([1, 2, "aasf", "1", "123", 123]))
+# print(filter_list2([]))
