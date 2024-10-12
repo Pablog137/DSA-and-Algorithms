b6d9e9afab6a24f417feac64e5a8417a54667411
Pablo <pablogutierrezvaldin@gmail.com>
2024-10-12 10:21:43 +0200

    Exercise 5

--- Exercises/Exercises2.py
+++ Exercises/Exercises2.py
@@ -1,4 +1,3 @@
-
 ### ! EXERCISE 4
 
 """Matryoshka dolls are traditionally wooden dolls that can be nested
@@ -49,3 +48,58 @@ def matryoshka(nested_list: list):
     return True
 
 
+### ! EXERCISE 5
+
+"""You probably know the "like" system from Facebook and other pages. 
+People can "like" blog posts, pictures or other items. We want to create 
+the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item.
+It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+"""
+
+
+# Solution 1
+
+
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return f"{names[0]} likes this"
+    elif len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif len(names) == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+
+# Solution 2
+
+
+def likes2(names: list[str]) -> str:
+    match len(names):
+        case 0:
+            return "no one likes this"
+        case 1:
+            return f"{names[0]} likes this"
+        case 2:
+            return f"{names[0]} and {names[1]} like this"
+        case 3:
+            return f"{names[0]}, {names[1]} and {names[2]} like this"
+        case _:
+            return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+
+# print(likes(["Peter", "Jacob", "Sara", "Juan", "Pepe"]))
