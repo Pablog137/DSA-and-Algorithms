4aed20bc75f87936b59a5f5da8189684c8dcf2ee
Pablo <pablogutierrezvaldin@gmail.com>
2024-10-17 16:50:02 +0200

    Queue

---
+++ Stacks & Queues/Queue.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            if temp == self.first and temp == self.last:
+                print(f"{temp.value} (First/Last)")
+            elif temp == self.first:
+                print(f"{temp.value} (First)")
+            elif temp == self.last:
+                print(f"{temp.value} (Last)")
+            else:
+                print(temp.value)
+            temp = temp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return True
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            temp = self.first
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
+
+my_queue = Queue(4)
+my_queue.enqueue(5)
+my_queue.enqueue(6)
+my_queue.dequeue()
+my_queue.print_queue()
